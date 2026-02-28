f9d364638e826ee6c239ff422dc30c81cd95541b
Maaz-Bin-Haider <maazbinhaider2024@gmail.com>
2026-02-28 15:40:53 +0500

    completed Py crash course Part1

---
+++ Refactored_Py_DS_ML_Bootcamp-master/01-Python-Crash-Course/basic_strimg_formatting.py
@@ -0,0 +1,10 @@
+#String Formatting
+name = 'Maaz'
+seat = 'B23110106038'
+
+print("My name is {} and my Seta No. is {}".format(name,seat))
+
+country = 'Pakistan'
+city = 'karachi'
+
+print('My country name is {one}, In {one} I lived in {two}'.format(one=country,two=city))
